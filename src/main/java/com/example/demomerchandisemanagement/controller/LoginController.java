5919141d5b5b776f8ceaca3b7f3b26a50d15e423
hayahayao <azurecho13@gmail.com>
2023-08-22 15:47:16 +0800

    feat(login): mvc

--- src/main/java/com/example/demomerchandisemanagement/controller/LoginController.java
+++ src/main/java/com/example/demomerchandisemanagement/controller/LoginController.java
@@ -1,14 +1,30 @@
 package com.example.demomerchandisemanagement.controller;
 
+import com.example.demomerchandisemanagement.form.LoginForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-@RequestMapping("/login")
 public class LoginController {
-    @GetMapping
-    public String view() {
+    // 仮の定数
+    private static final String LOGIN_ID = "user";
+    private static final String PASSWORD = "pwd";
+
+    @GetMapping("/login")
+    public String view(Model model, LoginForm form) {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(Model model, LoginForm form) {
+        var isCorrentUserAuth = form.getLoginId().equals(LOGIN_ID) && form.getPassword().equals(PASSWORD);
+        if (isCorrentUserAuth) {
+            return "redirect:/menu";
+        } else {
+            model.addAttribute("errorMsg", "ログインIDとパスワードの組み合わせが間違っています。");
+            return "login";
+        }
     }
 }
---
+++ src/main/java/com/example/demomerchandisemanagement/form/LoginForm.java
@@ -0,0 +1,10 @@
+package com.example.demomerchandisemanagement.form;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+    private String loginId;
+    private String password;
+
+}
